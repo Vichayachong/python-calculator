cfbdca70589878aa178609f2551c68b06a24f06e
Vichaya.cho <109650333+Vichayachong@users.noreply.github.com>
2024-11-11 13:49:26 +0000

    fix the error and added test case.

--- test_calculator.py
+++ test_calculator.py
@@ -10,6 +10,35 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(1, 2), 3)
 
     # Add the following test methods to the TestCalculator class:
+    def test_add1(self):
+        self.assertEqual(self.calc.add(3, 4),7)
+
+    def test_add2(self):
+        self.assertEqual(self.calc.add(-2, -3), -5)
+
+    def test_subtract1(self):
+        self.assertEqual(self.calc.subtract(8, 6), 2)
+
+    def test_subtract2(self):
+        self.assertEqual(self.calc.subtract(2, 12), -10)
+
+    def test_multiply1(self):
+        self.assertEqual(self.calc.multiply(3, 6), 18)
+
+    def test_multiply2(self):
+        self.assertEqual(self.calc.multiply(8, 9), 72)
+
+    def test_divide1(self):
+        self.assertEqual(self.calc.divide(21, 7),3)
+
+    def test_divide2(self):
+        self.assertEqual(self.calc.divide(9, 2), 4)
+
+    def test_modulo1(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+
+    def test_modulo2(self):
+        self.assertEqual(self.calc.modulo(9, 3), 0)
 
 if __name__ == '__main__':
     unittest.main()
